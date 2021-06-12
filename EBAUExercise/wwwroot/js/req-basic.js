f0de1280f26b552e79017a30c1d2f0971e446945
Tyrone Johnston <tyrone.johnston@hotmail.com>
2021-06-12 15:01:20 +1000

    Display - IOC Exercise
    Displayed 'IOC Exercise' endpoint result.

--- EBAUExercise/wwwroot/js/req-basic.js
+++ EBAUExercise/wwwroot/js/req-basic.js
@@ -7,3 +7,27 @@ function sendHelloWorldRequest()
 		document.getElementById("outputContainer").innerHTML = outputText;
 	});
 }
+
+
+function sendIocRequest()
+{
+	fetch("/tasks")
+	.then(response => response.json())
+	.then(function(data)
+	{
+		var containerElement = document.getElementById("outputContainer");
+		containerElement.innerHTML = "";
+		addTaskField("Saved", data.isDataSaved, containerElement);
+		addTaskField("Increment Count", data.count, containerElement);
+	});
+}
+
+
+function addTaskField(propName, propVal, containObj)
+{
+	var fieldElement = document.createElement("p");
+	var nameHTML = "<strong>" + propName + ": </strong>";
+	
+	fieldElement.innerHTML = nameHTML + propVal;
+	containObj.appendChild(fieldElement);
+}
--- changelog.md
+++ changelog.md
@@ -1,4 +1,3 @@
 # Changelog
 
-* Displayed 'Hello World' endpoint result.
-* HTTP request scripting for simple endpoints is found in 'req-basic.js'
+Displayed 'IOC Exercise' endpoint result.
